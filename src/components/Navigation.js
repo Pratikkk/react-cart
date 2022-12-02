f1365a76706b7392795ddf97be21c65268d6b7d3
Pratikkk <kenipratik9@gmail.com>
2022-12-02 17:05:45 +0530

    added tailwind css

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,8 +3,26 @@ import { Link } from "react-router-dom";
 const Navigation = () => {
     return (
         <>
-            <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
+           <nav className="container mx-auto flex items-center justify-between py-4">
+
+                   <Link to="/">
+                       <img style={ {height: 45} } src="/images/logo.png" alt="logo" />
+                   </Link>
+               <ul className="flex items-center">
+                   <li> <Link  to="/" >Home </Link> </li>
+                   <li> <Link  to="/products" >Products</Link> </li>
+                   <li>
+                       <Link to="/cart">
+                           <div>
+                               <span>10</span>
+                               <img src="/images/cart.png" alt="cart-icon"/>
+                           </div>
+                       </Link>
+                   </li>
+               </ul>
+
+               
+           </nav>
         </>
     )
 }
